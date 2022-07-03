c0f986fb3d1db673eb99f2e971110b5164280304
{tdoke2000} <{take.2000131@icloud.com}>
2022-07-03 13:04:23 +0900

    遷移

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import "daily_page.dart";
+import "weekly_page.dart";
 import 'weekly_task.dart';
 import 'daily_task.dart';
 
@@ -17,9 +19,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(List_Weekly, List_Daily),
+      home: MyHomePage(
+        title: "HomePage",
+      ),
       routes: {
-        "/Home": (context) => Home(List_Weekly, List_Daily),
+        "/WeeklyPage": (context) => WeeklyPage(List_Weekly),
+        "/DailyPage": (context) => DailyPage(List_Daily),
         "/WeeklyTask": (BuildContext context) => WeeklyTask(),
         "/DailyTask": (BuildContext context) => DailyTask(),
       },
@@ -27,111 +32,50 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatefulWidget {
-  Home(this.List_Weekly, this.List_Daily);
-  final List<List<String>> List_Weekly;
-  final List<List<String>> List_Daily;
-
-  // Map List_Weekly;
-  // Map List_Daily;
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
 
   @override
-  State<Home> createState() => _HomeState(List_Weekly, List_Daily);
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _HomeState extends State<Home> {
-  _HomeState(this.List_Weekly, this.List_Daily);
-  final List<List<String>> List_Weekly;
-  final List<List<String>> List_Daily;
+class _MyHomePageState extends State<MyHomePage> {
+  late PageController _pageController;
+  int _selectedIndex = 0;
+
+  // ボトムメニューの遷移先画面
+  var _pages = [
+    WeeklyPage(List_Weekly),
+    DailyPage(List_Daily),
+  ];
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _selectedIndex);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
+  void _onPageChanged(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    //final String args = ModalRoute.of(context).settings.arguments;
-    List<List<String>> List_Day = [
-      ["", ""]
-    ];
-    List<List<String>> List_wek;
-
-    List_Day.addAll(List_Daily);
+    //return LoginPage();
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('top page'),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            DrawerHeader(
-              child: Text(
-                'My App',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
-                ),
-              ),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-            ),
-            ListTile(
-              title: Text('今日のto do選択'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text("今日のto do追加"),
-              onTap: () {
-                Navigator.of(context).pushNamed("/DailyTask");
-              },
-            ),
-            ListTile(
-              title: Text('1週間のto doの作成'),
-              onTap: () {
-                Navigator.of(context).pushNamed("/WeeklyTask");
-              },
-            ),
-            ListTile(
-              title: Text('1週間のto doの編集'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        mainAxisSpacing: 5,
-        //crossAxisSpacing: 1,
-        childAspectRatio: 0.3,
-        children: [
-          ListView.builder(
-            itemCount: List_Daily.length - 1, // moviesの長さだけ表示
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                child: ListTile(
-                  title: Text('科目${List_Daily[index + 1][0]}'), // 科目
-                  subtitle: Text('単元:${List_Daily[index + 1][1]}'), // 単元
-                  trailing: Text('1/${List_Daily[index + 1][2]}'),
-                ),
-              );
-            },
-          ),
-          ListView.builder(
-            itemCount: List_Weekly.length - 1, // moviesの長さだけ表示
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                child: ListTile(
-                  title: Text('科目${List_Weekly[index + 1][0]}'), // 科目
-                  subtitle: Text('単元:${List_Weekly[index + 1][1]}'), // 単元
-                  trailing: Text('1/${List_Weekly[index + 1][2]}'),
-                ),
-              );
-            },
-          ),
-        ],
-      ),
+      body: PageView(
+          controller: _pageController,
+          onPageChanged: _onPageChanged,
+          children: _pages),
     );
   }
 }
